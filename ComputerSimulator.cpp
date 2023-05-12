ef71b2dd6052b880ba5644e27cb3a779a57618c9
Parker McLeod <64102246+ParkerCMcLeod@users.noreply.github.com>
2023-05-12 10:17:50 -0400

    Fixed scoping issues with passing by reference (thanks Cris)

--- ComputerSimulator.cpp
+++ ComputerSimulator.cpp
@@ -113,13 +113,14 @@ public:
 class FullAdder {
 private:
     HalfAdder halfAdder1;
+    Input halfAdder1Sum;
     HalfAdder halfAdder2;
 
 public:
     FullAdder(const Gate& carryInput, const Gate& input1, const Gate& input2)
-        : halfAdder1(input1, input2),
-          halfAdder2(Input(halfAdder1.sum()), carryInput)
-    {}
+        : halfAdder1(input1, input2), halfAdder1Sum(halfAdder1.sum()),
+          halfAdder2(halfAdder1Sum, carryInput)
+        {}
 
     bool sum() const {
         return halfAdder1.sum() ^ halfAdder2.sum();
@@ -150,12 +151,19 @@ private:
     const Gate& inputB1;
     const Gate& inputB0;
     HalfAdder adder0;
+    Input adder0Carry;
     FullAdder adder1;
+    Input adder1Carry;
     FullAdder adder2;
+    Input adder2Carry;
     FullAdder adder3;
+    Input adder3Carry;
     FullAdder adder4;
+    Input adder4Carry;
     FullAdder adder5;
+    Input adder5Carry;
     FullAdder adder6;
+    Input adder6Carry;
     FullAdder adder7;
 
     public:
@@ -168,13 +176,20 @@ private:
         inputB7(inputB7), inputB6(inputB6), inputB5(inputB5), inputB4(inputB4),
         inputB3(inputB3), inputB2(inputB2), inputB1(inputB1), inputB0(inputB0),
         adder0(inputA0, inputB0),
-        adder1(Input(adder0.carry()), inputA1, inputB1),
-        adder2(Input(adder1.carry()), inputA2, inputB2),
-        adder3(Input(adder2.carry()), inputA3, inputB3),
-        adder4(Input(adder3.carry()), inputA4, inputB4),
-        adder5(Input(adder4.carry()), inputA5, inputB5),
-        adder6(Input(adder5.carry()), inputA6, inputB6),
-        adder7(Input(adder6.carry()), inputA7, inputB7)
+        adder0Carry(adder0.carry()),
+        adder1(adder0Carry, inputA1, inputB1),
+        adder1Carry(adder1.carry()),
+        adder2(adder1Carry, inputA2, inputB2),
+        adder2Carry(adder2.carry()),
+        adder3(adder2Carry, inputA3, inputB3),
+        adder3Carry(adder3.carry()),
+        adder4(adder3Carry, inputA4, inputB4),
+        adder4Carry(adder4.carry()),
+        adder5(adder4Carry, inputA5, inputB5),
+        adder5Carry(adder5.carry()),
+        adder6(adder5Carry, inputA6, inputB6),
+        adder6Carry(adder6.carry()),
+        adder7(adder6Carry, inputA7, inputB7)
         {}
 
     bool evaluate0() {
@@ -232,108 +247,6 @@ private:
     }
 };
 
-class RippleCarrySubtractor {
-private:
-    const Gate& inputA7;
-    const Gate& inputA6;
-    const Gate& inputA5;
-    const Gate& inputA4;
-    const Gate& inputA3;
-    const Gate& inputA2;
-    const Gate& inputA1;
-    const Gate& inputA0;
-    const Gate& inputB7;
-    const Gate& inputB6;
-    const Gate& inputB5;
-    const Gate& inputB4;
-    const Gate& inputB3;
-    const Gate& inputB2;
-    const Gate& inputB1;
-    const Gate& inputB0;
-    HalfAdder adder0;
-    FullAdder adder1;
-    FullAdder adder2;
-    FullAdder adder3;
-    FullAdder adder4;
-    FullAdder adder5;
-    FullAdder adder6;
-    FullAdder adder7;
-
-public:
-    RippleCarrySubtractor(const Gate& inputA7, const Gate& inputA6, const Gate& inputA5, const Gate& inputA4,
-                          const Gate& inputA3, const Gate& inputA2, const Gate& inputA1, const Gate& inputA0,
-                          const Gate& inputB7, const Gate& inputB6, const Gate& inputB5, const Gate& inputB4,
-                          const Gate& inputB3, const Gate& inputB2, const Gate& inputB1, const Gate& inputB0)
-        : inputA7(inputA7), inputA6(inputA6), inputA5(inputA5), inputA4(inputA4),
-        inputA3(inputA3), inputA2(inputA2), inputA1(inputA1), inputA0(inputA0),
-        inputB7(inputB7), inputB6(inputB6), inputB5(inputB5), inputB4(inputB4),
-        inputB3(inputB3), inputB2(inputB2), inputB1(inputB1), inputB0(inputB0),
-        adder0(inputA0, NotGate(inputB0)), // invert all B bits
-        adder1(Input(adder0.carry()), inputA1, NotGate(inputB1)),
-        adder2(Input(adder1.carry()), inputA2, NotGate(inputB2)),
-        adder3(Input(adder2.carry()), inputA3, NotGate(inputB3)),
-        adder4(Input(adder3.carry()), inputA4, NotGate(inputB4)),
-        adder5(Input(adder4.carry()), inputA5, NotGate(inputB5)),
-        adder6(Input(adder5.carry()), inputA6, NotGate(inputB6)),
-        adder7(Input(adder6.carry()), inputA7, NotGate(inputB7))
-        {}
-
-        bool evaluate0() {
-            bool sum0 = adder0.sum();
-            return sum0;
-        }
-
-        bool evaluate1() {
-            bool sum1 = adder1.sum();
-            return sum1;
-        }
-
-        bool evaluate2() {
-            bool sum2 = adder2.sum();
-            return sum2;
-        }
-
-        bool evaluate3() {
-            bool sum3 = adder3.sum();
-            return sum3;
-        }
-
-        bool evaluate4() {
-            bool sum4 = adder4.sum();
-            return sum4;
-        }
-
-        bool evaluate5() {
-            bool sum5 = adder5.sum();
-            return sum5;
-        }
-
-        bool evaluate6() {
-            bool sum6 = adder6.sum();
-            return sum6;
-        }
-
-        bool evaluate7() {
-            bool sum7 = adder7.sum();
-            return sum7;
-        }
-
-        bool borrow() {
-            bool borrowValue = NotGate(Input(adder7.carry())).evaluate();
-            return borrowValue;
-        }
-
-        void output() {
-            if (borrow()) {
-                throw std::runtime_error("UNDERFLOW");
-            } else {
-                printf("%d%d%d%d%d%d%d%d", static_cast<int>(evaluate7()), static_cast<int>(evaluate6()),
-                    static_cast<int>(evaluate5()), static_cast<int>(evaluate4()), static_cast<int>(evaluate3()),
-                    static_cast<int>(evaluate2()), static_cast<int>(evaluate1()), static_cast<int>(evaluate0()));
-            }
-        }
-};
-
 int main() {
     Input one(true);
     Input zero(false);
@@ -343,9 +256,6 @@ int main() {
     XorGate xorGate1(zero, one);
     RippleCarryAdder rippleCarryAdder(andGate1, zero, zero, zero, zero, zero, zero, orGate0, zero, zero, zero, zero, zero, zero, zero, xorGate1); // 00000001 + 00000001
     rippleCarryAdder.output(); // 00000010
-
-    RippleCarrySubtractor rippleCarrySubtractor(zero, zero, zero, zero, zero, one, zero, zero, zero, zero, zero, zero, zero, zero, zero, one); // 00000100 - 00000001
-    rippleCarrySubtractor.output(); // 00000011
 
     return 0;
 }
